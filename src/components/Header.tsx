dd579f4a40d2c728f2fe26739e99ccdd5f582bc1
artartur <arturgfs@hotmail.com>
2023-09-04 03:49:07 -0300

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import React from "react";
+
+import "../styles/Header.scss";
+
+export default function Header({ children, title }: HeaderProps) {
+  return (
+    <header className="page-header">
+      <div className="top-bar-container">
+        <Link className="alignment" id="backIcon" to="/">
+          <IoIosArrowRoundBack size="4rem" />
+          Voltar
+        </Link>
+        <h1>Smarthub</h1>
+      </div>
+
+      <div className="header-content">
+        <strong>{title}</strong>
+        {children}
+      </div>
+    </header>
+  );
+}
+
+type HeaderProps = {
+    children?: JSX.Element | JSX.Element[] | string;
+    title: string;
+  };
