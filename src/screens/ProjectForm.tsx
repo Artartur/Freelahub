cb48b334421f154c69a39d25a29624d78d411327
artartur <artur.souza@liferay.com>
2023-09-08 18:11:35 -0300

    Describe and style the structure of the ProjectForm

--- src/screens/ProjectForm.tsx
+++ src/screens/ProjectForm.tsx
@@ -1,10 +1,22 @@
 import Header from "../components/Header";
 import React from "react";
 
+import "../styles/ProjectForm.scss";
+
 export default function ProjectForm() {
   return (
     <div id="page-project-form" className="container">
-      <Header title="Que incrivel que voce quer comecar seu projeto conosco."/>
+      <Header
+        description="O primeiro passo, é preencher esse formulario de inscrição de seu projeto"
+        title="Que incrível que voce quer começar seu projeto conosco."
+      />
+      <main>
+        <form>
+          <fieldset>
+            <legend>Seus Dados</legend>
+          </fieldset>
+        </form>
+      </main>
     </div>
   );
 }
